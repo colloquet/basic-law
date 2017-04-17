dbded7e33fae9dddda8d8a3ee7ed6235a0c2a069
Colloque Tsui <colloquet@gmail.com>
2017-04-17 22:21:07 +0800

    Added Facebook share

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="site-footer">
+  <div class="uk-text-center uk-margin-large-bottom site-footer">
     <hr>
-    <p class="uk-text-center uk-text-muted">Made in Hong Kong by <a href="http://colloquet.github.io/" target="_blank">Colloque Tsui</a></p>
+    <p class="uk-text-muted">Made in Hong Kong by <a href="http://colloquet.github.io/" target="_blank">Colloque Tsui</a></p>
+    <div class="fb-like" data-href="https://basiclaw.hk/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
   </div>
 </template>
