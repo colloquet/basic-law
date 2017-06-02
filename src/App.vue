e32de110f86596e882a402396dbd96732e1eb0ea
Colloque Tsui <colloquet@gmail.com>
2017-06-03 02:15:02 +0800

    Added open source msg

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,10 @@
 
       <div class="uk-text-center uk-margin-large-bottom site-footer">
         <hr>
-        <p class="uk-text-muted">Made in Hong Kong by <a href="http://colloquet.github.io/" target="_blank">Colloque Tsui</a></p>
+        <p class="uk-text-muted">
+          Made in Hong Kong by <a href="http://colloquet.github.io/" target="_blank">Colloque Tsui</a><br>
+          Source code available at <a href="https://github.com/colloquet/basic-law" target="_blank">GitHub</a>
+        </p>
         <div class="fb-like" data-href="https://basiclaw.hk/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
       </div>
 
